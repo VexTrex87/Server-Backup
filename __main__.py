4fcbcf82c559dc83336f735b0468b43c2f4d2684
Vex87 <AndrewKyleGuilas@gmail.com>
2021-07-12 21:17:12 -0400

    revamp

--- __main__.py
+++ __main__.py
@@ -1,109 +1,19 @@
+import discord
 from discord.ext import commands
 import dotenv
 import os
 
 dotenv.load_dotenv('.env')
-TOKEN = os.getenv('TOKEN')
 client = commands.Bot(command_prefix='$')
 guild = None
 
-async def copy_channels(guild, backup_guild, messages=True):
-    for channel in backup_guild.channels:
-        print(f'Deleting channel #{channel.name}...')
-        await channel.delete()
-        print(f'Deleted channel #{channel.name}')
-
-    for category in guild.categories:
-        print(f'Creating category {category.name}...')
-        new_category = await backup_guild.create_category(
-            name=category.name, position=category.position
-        )
-        
-        for text_channel in category.text_channels:
-            print(f'Creating text channel #{text_channel.name}...')
-            new_text_channel = await new_category.create_text_channel(
-                name=text_channel.name, 
-                position=text_channel.position
-            )
-            
-            if not messages:
-                continue
-
-            webhooks = {}
-            messages = await text_channel.history(limit=None, oldest_first=True).flatten()
-            count = len(messages)
-            for i, message in enumerate(messages):
-                webhook = webhooks.get(message.author.name)
-                if not webhook:
-                    webhook = await new_text_channel.create_webhook(name=message.author.name)
-                    webhooks[message.author.name] = webhook
-
-                content = message.content
-                embeds = message.embeds
-                files = [await a.to_file() for a in message.attachments]
-                username = message.author.name
-                avatar_url = message.author.avatar_url
-                
-                for embed in embeds:
-                    if embed.type == 'gifv':
-                        embeds = []
-
-                print(f'Sending message {message.id} in #{text_channel.name}...')
-
-                try:
-                    await webhook.send(
-                        content=content, 
-                        embeds=embeds, 
-                        files=files, 
-                        username=username, 
-                        avatar_url=avatar_url
-                    )
-                except:
-                    pass
-
-                percent = round(i / count * 100, 2)
-                print(f'Sent message {message.id} in #{text_channel.name} ({i}/{count} - {percent}%)')
-
-            print(f'Created text channel #{text_channel.name}')
-
-        for voice_channel in category.voice_channels:
-            print(f'Creating voice channel #{voice_channel.name}...')
-            await new_category.create_voice_channel(
-                name=voice_channel.name,
-                position=voice_channel.position
-            )
-            print(f'Created voice channel #{voice_channel.name}')
-
-        print(f'Created category {category.name}')
-
-async def copy_roles(guild, backup_guild):
-    for role in backup_guild.roles:
-        if role.is_default() or role.is_bot_managed() or role.is_integration():
-            continue
-
-        print(f'Deleting role @{role.name}...')
-        await role.delete()
-        print(f'Deleted role @{role.name}')
-
-    for role in guild.roles[::-1]:
-        if role.is_default() or role.is_bot_managed() or role.is_integration():
-            continue
-
-        print(f'Creating role @{role.name}...')
-        new_role = await backup_guild.create_role(
-            name=role.name, 
-            colour=role.color, 
-            permissions=role.permissions,
-            hoist=role.hoist,
-            mentionable=role.mentionable,
-        )
-
+def get_object(objects, value):
+    for obj in objects:
         try:
-            await new_role.edit(position=role.position)
+            if obj.name == value or value == obj.mention or str(obj.id) in value or obj.id == int(value):
+                return obj
         except:
             pass
-
-        print(f'Created role @{role.name}')
 
 @client.event
 async def on_ready():
@@ -121,32 +31,52 @@ async def copy(context):
     await context.send(f'Copied {guild.name}')
 
 @client.command()
-async def paste(context, flag=None):
+async def paste(context):
     if not guild:
         await context.send('ERROR: No guild copied')
         return
     
-    if flag == 'all':
-        print(f'Pasting {guild.name}...')
-        await copy_roles(guild, context.guild)
-        await copy_channels(guild, context.guild)
-        print(f'Pasted {guild.name}')
-    elif flag == 'channels':
-        print(f'Pasting {guild.name} channels...')
-        await copy_channels(guild, context.guild, messages=False)
-        print(f'Pasted {guild.name} channels')
-    elif flag == 'messages':
-        print(f'Pasting {guild.name} messages...')
-        await copy_channels(guild, context.guild, messages=True)
-        print(f'Pasted {guild.name} messages')
-    elif flag == 'roles':
-        await context.send(f'Pasting {guild.name} roles...')
-        await copy_roles(guild, context.guild)
-        await context.send(f'Pasted {guild.name} roles')
-    elif flag == 'settings':
-        await context.send(f'Pasting {guild.name} settings...')
-        await context.send(f'Pasted {guild.name} settings')
-    else:
-        await context.send('ERROR: Invalid flag')  
+    await context.send(f'Coping {guild.name}...')
+    server_messages = {}
+    count = 0
 
-client.run(TOKEN)
+    for channel in guild.text_channels:
+        backup_channel = get_object(context.guild.text_channels, channel.name)
+        messages = await channel.history(limit=None, oldest_first=True).flatten()
+        count += len(messages)
+        server_messages[backup_channel] = messages
+
+    copied_messages = 0
+    rounded_percent = 0
+    for channel, messages in server_messages.items():
+        webhook = await channel.create_webhook(name=channel.name)
+        for message in messages:
+            copied_messages += 1
+
+            embeds = message.embeds
+            for embed in embeds:
+                if embed.type == 'gifv':
+                    embeds = []
+                    break
+
+            try:
+                await webhook.send(
+                    content=message.content, 
+                    embeds=embeds, 
+                    files=[await a.to_file() for a in message.attachments], 
+                    username=message.author.name, 
+                    avatar_url=message.author.avatar_url
+                )
+            except:
+                pass
+
+            percent = round(copied_messages / count * 100, 2)
+            print(f'Sent message {message.id} in #{channel.name} ({copied_messages}/{count} - {percent}%)')
+            if round(percent) != int(rounded_percent):
+                rounded_percent = str(round(percent))
+                await client.change_presence(activity=discord.Game(f'{rounded_percent}%'))
+
+    await context.send(f'Copied {guild.name}')
+    await client.change_presence(activity=discord.Game(f'Done!'))
+
+client.run(os.getenv('TOKEN'))
